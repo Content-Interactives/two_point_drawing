fe8e72627a4e51732494a42db116c5e1158db1b3
User <user@example.com>
2026-02-18 14:15:44 -0800

    added app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TwoPointDrawing from './components/TwoPointDrawing'
+
+
+function App() {
+  return (
+      <div className="App">
+        <TwoPointDrawing />
+      </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/TwoPointDrawing.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+const TwoPointDrawing = () => {
+
+
+	return (
+        <div>test</div>
+)
+};
+
+
+export default TwoPointDrawing;
